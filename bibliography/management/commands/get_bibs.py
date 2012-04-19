0a8678923acde7ab3dc479ba32b9b2fa84789abb
Tim McNamara <code@timmcnamara.co.nz>
2012-04-19 14:45:54 +1200

    Turn get_bibs.py into a manage.py command

--- bibliography/management/commands/get_bibs.py
+++ bibliography/management/commands/get_bibs.py
@@ -1,40 +1,33 @@
 # Script to get bibliography records from www.dragonfly.co.nz
 
+import re
 import sys
 import os
 import shutil
 import optparse
+from optparse import make_option
 sys.path.append('/usr/local/django')
 
 from django.core.management import setup_environ
-from www import settings
+from django.core.management.base import BaseCommand, CommandError
 
-setup_environ(settings)
+from bibliography.models import Reference
 
-import re
-from www.bibliography.models import Reference
+class GetBibsCommand(BaseCommand):
+    option_list = BaseCommand.option_list + (make_option('-o', '--output', dest='output', help='Output file'), )
+    option_list = BaseCommand.option_list + (make_option('-t', '--tags', dest='tags', action='append', help='Filter references with specified tags'), )
+    self.can_import_settings = True
 
-parser = optparse.OptionParser()
-parser.add_option('-o', '--output', dest='output', help='Output file')
-parser.add_option('-t', '--tags', dest='tags', action='append', help='Filter references with specified tags')
-options, remainder=parser.parse_args()
+    def handle(self, *args, **options):
+        from django.conf import settings
+        setup_environ(settings)
 
-refs = Reference.objects.all()
-if options.tags:
-    for t in options.tags:
-        refs = refs.filter(tags__name = t)
+        refs = Reference.objects.all()
+        if options.tags:
+            for t in options.tags:
+                refs = refs.filter(tags__name = t)
 
-bibs = []
-for r in refs:
-    bibs.append(r.bibtex)
-bib = '\n\n'.join(bibs)
+        bib = '\n\n'.join(r.bibtex for r in refs)
 
-if options.output:
-    f = open(options.output, 'w')
-else:
-    f = open('references.bib', 'w')
-f.write(bib)
-f.close()
-
-
-
+        with open(options.output or 'references.bib', 'w') as f:
+            f.write(bib)
