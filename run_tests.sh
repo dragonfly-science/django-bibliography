9da4ab8aa2463b3a479a17c87937a7d49bf88feb
Joel Pitt <joel@joelpitt.com>
2013-12-04 11:47:52 +1300

    return the right exit code

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 # We have to run using coverage because south migrations short circuit the nose
 # coverage plugin and results in incorrect coverage reports.
 coverage run manage.py test $@
+RET=$?
 coverage html --include=bibliography\*
 coverage xml --include=bibliography\*
+exit $RET
